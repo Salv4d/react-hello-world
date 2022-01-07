054ae075bc7e6a47ab6ecef782b30817fbdb0284
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-01-06 22:40:41 -0400

    change component default properties

--- src/index.js
+++ src/index.js
@@ -3,25 +3,31 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-class HelloComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, dear component!</h1>
-        <h2>My name is {this.props.name}.</h2>
-      </div>
-    );
-  }
-}
+// class HelloComponent extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, dear component!</h1>
+//         <h2>My name is {this.props.name}.</h2>
+//       </div>
+//     );
+//   }
+// }
 
-// const HelloComponent = function (props) {
-// return (
-//   <div>
-//     <h1>Hello, dear component!</h1>
-//     <h2>My name is {props.name}.</h2>
-//   </div>
-// );
-// };
+const HelloComponent = function (props) {
+  return (
+    <div>
+      <h1>Hello, dear component!</h1>
+      <h2>
+        My name is {props.name}. I'm a {props.role}
+      </h2>
+    </div>
+  );
+};
+
+HelloComponent.defaultProps = {
+  role: "Software Developer",
+};
 
 ReactDOM.render(
   <HelloComponent name="Salvador" />,
