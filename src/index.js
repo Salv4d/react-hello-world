949a9b2d22e39778d263853966f4b75af08d8a99
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-01-07 16:23:20 -0400

    add a input with a counter that updates while the user types

--- src/index.js
+++ src/index.js
@@ -3,34 +3,68 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-// class HelloComponent extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello, dear component!</h1>
-//         <h2>My name is {this.props.name}.</h2>
-//       </div>
-//     );
-//   }
-// }
+class HelloComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, dear component!</h1>
+        <h2>
+          My name is {this.props.name}. I'm a {this.props.role}
+        </h2>
+      </div>
+    );
+  }
+}
 
-const HelloComponent = function (props) {
-  return (
-    <div>
-      <h1>Hello, dear component!</h1>
-      <h2>
-        My name is {props.name}. I'm a {props.role}
-      </h2>
-    </div>
-  );
-};
+// const HelloComponent = function (props) {
+//   return (
+//     <div>
+//       <h1>Hello, dear component!</h1>
+//       <h2>
+//         My name is {props.name}. I'm a {props.role}
+//       </h2>
+//     </div>
+//   );
+// };
 
 HelloComponent.defaultProps = {
   role: "Software Developer",
 };
 
+class InputCounter extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  onTextChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  render() {
+    const text = "text" in this.state ? this.state.text : this.props.text;
+    return (
+      <div>
+        <textarea
+          defaultValue={text}
+          onChange={(event) => this.onTextChange(event)}
+        ></textarea>
+        <h3>{text.length}</h3>
+      </div>
+    );
+  }
+}
+
+InputCounter.defaultProps = {
+  text: "Temet Nosce",
+};
+
 ReactDOM.render(
-  <HelloComponent name="Salvador" />,
+  <div>
+    <HelloComponent name="Salvador" />,<InputCounter></InputCounter>
+  </div>,
   document.getElementById("root")
 );
 
